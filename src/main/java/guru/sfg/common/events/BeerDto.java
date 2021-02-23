4ecf9e5bd271366b50e35ec09fdb2b0e1038709b
Nathan Lehman <nlehman@dlcouch.com>
2021-02-23 17:01:19 -0500

    New inventory listener

---
+++ src/main/java/guru/sfg/common/events/BeerDto.java
@@ -0,0 +1,54 @@
+package guru.sfg.common.events;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto implements Serializable {
+
+    static final long serialVersionUID = -9155147871471697256L;
+
+    @Null
+    private UUID id;
+
+    @Null
+    private Integer version;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+    @Null
+    private OffsetDateTime createdDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+    @Null
+    private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String beerName;
+
+    @NotNull
+    private String beerStyle;
+
+    @NotNull
+    private String upc;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    @Positive
+    @NotNull
+    private BigDecimal price;
+
+    @Positive
+    private Integer quantityOnHand;
+}
---
+++ src/main/java/guru/sfg/common/events/BeerEvent.java
@@ -0,0 +1,17 @@
+package guru.sfg.common.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BeerEvent {
+
+    static final long serialVersionUID = -8004823140984486493L;
+
+    private BeerDto beerDto;
+}
